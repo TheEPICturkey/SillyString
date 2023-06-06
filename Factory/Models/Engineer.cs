f38e892faf057252cec49522981418c3b992556b
Brandon Spear <Brandon.Spear97@gmail.com>
2023-06-05 20:13:37 -0700

    push start of files

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Factory.Models
+{
+  public class Engineer
+  {
+    public int EngineerId { get; set; }
+
+    [Required(ErrorMessage = "The engineer's name can't be empty!")]
+    public string EngineerName { get; set; }
+
+    [Required(ErrorMessage = "You must enter details about the Engineer.")]
+    public string EngineerDetails { get; set; }
+    public List<EngineerMachine> JoinEntities { get; }
+  }
+}
