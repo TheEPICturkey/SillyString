86230b6f6336dde14317b7578b5f96fd4c182f31
Brandon Spear <Brandon.Spear97@gmail.com>
2023-06-11 09:25:50 -0700

    add edits

--- brandon_spear_factory.sql
+++ brandon_spear_factory.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
 --
--- Host: localhost    Database: dr_sillystringz
+-- Host: localhost    Database: factory
 -- ------------------------------------------------------
 -- Server version	8.0.33
 
@@ -24,24 +24,28 @@ DROP TABLE IF EXISTS `__efmigrationshistory`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `__efmigrationshistory` (
   `MigrationId` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `ProductVersion` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `ProductVersion` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`MigrationId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `engineermachines1`
+-- Table structure for table `engineermachines`
 --
 
-DROP TABLE IF EXISTS `engineermachines1`;
+DROP TABLE IF EXISTS `engineermachines`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `engineermachines1` (
+CREATE TABLE `engineermachines` (
   `EngineerMachineId` int NOT NULL AUTO_INCREMENT,
   `EngineerId` int NOT NULL,
   `MachineId` int NOT NULL,
-  PRIMARY KEY (`EngineerMachineId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`EngineerMachineId`),
+  KEY `IX_EngineerMachines_EngineerId` (`EngineerId`),
+  KEY `IX_EngineerMachines_MachineId` (`MachineId`),
+  CONSTRAINT `FK_EngineerMachines_Engineers_EngineerId` FOREIGN KEY (`EngineerId`) REFERENCES `engineers` (`EngineerId`) ON DELETE CASCADE,
+  CONSTRAINT `FK_EngineerMachines_Machines_MachineId` FOREIGN KEY (`MachineId`) REFERENCES `machines` (`MachineId`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -68,8 +72,8 @@ DROP TABLE IF EXISTS `machines`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `machines` (
   `MachineId` int NOT NULL AUTO_INCREMENT,
-  `MachineName` longtext NOT NULL,
-  `MachineDescription` longtext NOT NULL,
+  `MachineName` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `MachineDescription` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`MachineId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -83,4 +87,4 @@ CREATE TABLE `machines` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-04 12:01:53
+-- Dump completed on 2023-06-11  9:24:50
